531c5d1fe594f01ed30040bf9b8e49dbdc1accb0
MD Misbah ur rahman <rahmanmisbahur2005@gmail.com>
2025-07-27 15:50:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for internal navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
